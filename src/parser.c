f3dd577a6abfc55171dffa3e7e4012ed2b5c97d8
Stephen Olesen <slepp@slepp.ca>
2026-02-25 23:24:04 -0700

    chore: regenerate parser from grammar.js

--- src/parser.c
+++ src/parser.c
@@ -63992,8 +63992,8 @@ TS_PUBLIC const TSLanguage *tree_sitter_hew(void) {
     .name = "hew",
     .max_reserved_word_set_size = 0,
     .metadata = {
-      .major_version = 0,
-      .minor_version = 1,
+      .major_version = 1,
+      .minor_version = 0,
       .patch_version = 0,
     },
   };
