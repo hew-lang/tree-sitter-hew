a1f1f94fbca85c1ec272262e65f9cfd485000ae4
Stephen Olesen <slepp@slepp.ca>
2026-09-03 09:49:38 -0600

    Sync grammar keywords with compiler syntax data

--- grammar.js
+++ grammar.js
@@ -726,8 +726,7 @@ export default grammar({
     // @sync:primitive_types
     primitive_type: $ => choice(
       'i8', 'i16', 'i32', 'i64', 'u8', 'u16', 'u32', 'u64', 'isize', 'usize',
-      'f32', 'f64', 'bool', 'char', 'string', 'bytes', 'void', 'duration',
-      'instant',
+      'f32', 'f64', 'bool', 'char', 'string', 'bytes', 'duration', 'instant',
     ),
 
     generic_type: $ => prec(1, seq($.identifier, $.type_arguments)),
